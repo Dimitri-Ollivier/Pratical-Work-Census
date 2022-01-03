f2de1bb6ce10b4b2333846434fc1b53f830e5031
dimit <dimitri.ollivier14@gmail.com>
2022-01-03 02:23:43 +0100

    Ajout des méthodes de recherche Villes

--- JavaRecensement/src/fr/epsi/b3/recensement/service/RechercheTopVilleDepartement.java
+++ JavaRecensement/src/fr/epsi/b3/recensement/service/RechercheTopVilleDepartement.java
@@ -1,6 +1,50 @@
 package fr.epsi.b3.recensement.service;
 
 import fr.epsi.b3.recensement.MenuService;
+import fr.epsi.b3.recensement.Recensement;
+import fr.epsi.b3.recensement.Ville;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RechercheTopVilleDepartement extends MenuService {
+    public static void traiter(Recensement recensement, Scanner scanner) {
+        System.out.println("Veulliez saisir le numéro de département de votre choix :");
+
+        String scan = scanner.next();
+        Boolean validCity = false;
+
+        Ville[] classement = new  Ville[10];
+        ArrayList<Ville> villes = recensement.listeVilles;
+
+        for (int i = 0; i <= classement.length - 1; i++) {
+            int max = 0;
+
+            for (int j = 0; j <= villes.size() - 1; j++) {
+                if (villes.get(j).getCodeRegion().equals(scan)) {
+                    if (villes.get(j).getPopulation() > max) {
+                        max = villes.get(j).getPopulation();
+                        classement[i] = villes.get(j);
+                        validCity = true;
+                    }
+                }
+            }
+
+            for (int k = 0; k <= villes.size() - 1; k++) {
+                if (villes.get(k).getPopulation() == max) {
+                    villes.remove(k);
+                }
+            }
+        }
+
+        if (!validCity) {
+            System.out.println("Le numéro de département saisie n'existe pas :(");
+        }
+
+        System.out.println("Voici les 10 premières villes les peuplées de France :");
+
+        for (int i = 0; i <= classement.length - 1; i++) {
+            System.out.println("    - " + classement[i].getNomCommune() + " dans le département numéro " + classement[i].getCodeDepartement() + " avec " + classement[i].getPopulation() + " habitants.");
+        }
+    }
 }
--- JavaRecensement/src/fr/epsi/b3/recensement/service/RechercheTopVilleFrance.java
+++ JavaRecensement/src/fr/epsi/b3/recensement/service/RechercheTopVilleFrance.java
@@ -2,7 +2,6 @@ package fr.epsi.b3.recensement.service;
 
 import fr.epsi.b3.recensement.MenuService;
 import fr.epsi.b3.recensement.Recensement;
-import fr.epsi.b3.recensement.Region;
 import fr.epsi.b3.recensement.Ville;
 
 import java.util.ArrayList;
@@ -12,32 +11,28 @@ public class RechercheTopVilleFrance extends MenuService {
     public static void traiter(Recensement recensement, Scanner scanner) {
         System.out.println("Voici les 10 premières villes les peuplées de France :");
 
-        ArrayList<String> villesValues = new ArrayList<>();
         Ville[] classement = new  Ville[10];
         ArrayList<Ville> villes = recensement.listeVilles;
 
         for (int i = 0; i <= classement.length - 1; i++) {
+            int max = 0;
+
             for (int j = 0; j <= villes.size() - 1; j++) {
-                int max = 0;
-
-                for (int j = 0; j <= regions.size() - 1; j++) {
-                    int population = regions.get(j).getPopulationRegion();
-                    if (population > max) {
-                        max = regions.get(j).getPopulationRegion();
-                        classement[i] = regions.get(j);
-                    }
+                if (villes.get(j).getPopulation() > max) {
+                    max = villes.get(j).getPopulation();
+                    classement[i] = villes.get(j);
                 }
+            }
 
-                for (int k = 0; k <= regions.size() - 1; k++) {
-                    if (regions.get(k).getPopulationRegion() == max) {
-                        regions.remove(k);
-                    }
+            for (int k = 0; k <= villes.size() - 1; k++) {
+                if (villes.get(k).getPopulation() == max) {
+                    villes.remove(k);
                 }
             }
         }
 
         for (int i = 0; i <= classement.length - 1; i++) {
-            System.out.println("La région " + classement[i].getNomRegion() + " avec " + classement[i].getPopulation() + " habitants.");
+            System.out.println("    - " + classement[i].getNomCommune() + " dans la région " + classement[i].getNomRegion() + " avec " + classement[i].getPopulation() + " habitants.");
         }
     }
 }
